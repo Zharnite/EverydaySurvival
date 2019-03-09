f14379c19a5359faba11d0dcdddcec52475e07b2
Zhen Yi Pan <zharnite@gmail.com>
2019-03-09 18:33:02 -0500

    init

---
+++ app/src/main/java/com/example/everydaysurvival/CPRFragment.java
@@ -0,0 +1,19 @@
+package com.example.everydaysurvival;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CPRFragment extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.cpr, container, false);
+        return myView;
+    }
+}
